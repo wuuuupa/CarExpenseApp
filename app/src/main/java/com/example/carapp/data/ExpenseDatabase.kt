1d9eabadf7e3b4b3db98ea888cec4fb652db1dc0
wuuuupa <sy51188544@gmail.com>
2025-06-25 15:36:22 +0800

    Create ExpenseDatabase.kt

---
+++ app/src/main/java/com/example/carapp/data/ExpenseDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.carapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Expense::class], version = 1, exportSchema = false)
+abstract class ExpenseDatabase : RoomDatabase() {
+    abstract fun expenseDao(): ExpenseDao
+    
+    companion object {
+        @Volatile
+        private var INSTANCE: ExpenseDatabase? = null
+        
+        fun getDatabase(context: Context): ExpenseDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ExpenseDatabase::class.java,
+                    "expense_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
