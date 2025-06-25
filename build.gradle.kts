8533a39e5faef32c31f507a4b43a78be2fdd3745
wuuuupa <sy51188544@gmail.com>
2025-06-25 17:56:18 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
         classpath("com.android.tools.build:gradle:8.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
